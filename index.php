7230003759a7b4d04dede9990a43056668c0ae6c
Your Name <farrasm227@gmail.com>
2025-10-25 10:55:34 +0700

    perubahan nama

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+</head>
+<body>
+    <nav class="flex justify-center items-center p-6 gap-6 bg-blue-500">
+        <h1 class="text-white font-bold text-2xl">Pertemuan 4 - php </h1>
+    </nav>
+</body>
+</html>
---
+++ script.js
@@ -0,0 +1,3 @@
+alert("hallo aku dari script.js");
+console.log("haloo");
+document.getElementsByid("judul").text ="haloooo";
